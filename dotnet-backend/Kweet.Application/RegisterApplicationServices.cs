06526134ab6931f1ce3d9133f5758e7b6a4dc700
Rayco Haex <raycoh1299@gmail.com>
2022-09-12 16:53:37 +0200

    Added base classes for configuration of program.cs

---
+++ dotnet-backend/Kweet.Application/RegisterApplicationServices.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Kweet.Application
+{
+    public static class RegisterApplicationServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
---
+++ dotnet-backend/Kweet.Infrastructure/RegisterInfrastructureServices.cs
@@ -0,0 +1,28 @@
+﻿using Kweet.Application.Contracts.Persistence;
+using Kweet.Infrastructure.Persistence;
+using Kweet.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kweet.Infrastructure
+{
+    public static class RegisterInfrastructureServices
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<KweetContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IKweetRepository, KweetRepository>();
+
+            return services;
+        }
+    }
+}
