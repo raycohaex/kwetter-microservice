e6327623f80ba9d772262cb6467fa8202716dfbd
Rayco Haex <raycoh1299@gmail.com>
2022-09-12 16:09:33 +0200

    Added boilerplate code for Domain and Persistance layer

---
+++ dotnet-backend/Kweet.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Kweet.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ dotnet-backend/Kweet.Domain/Entities/Kweet.cs
@@ -0,0 +1,15 @@
+﻿using Kweet.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kweet.Domain.Entities
+{
+    public class Kweet : EntityBase
+    {
+        public string UserName { get; set; }
+        public string TweetBody
+    }
+}
