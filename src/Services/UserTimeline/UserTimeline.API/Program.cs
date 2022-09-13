b0d3985c6d9c15b4e14e5c0ab75d98dc055d4239
Rayco Haex <raycoh1299@gmail.com>
2022-09-13 21:33:02 +0200

    Configured UserTimeline API

--- src/Services/UserTimeline/UserTimeline.API/Program.cs
+++ src/Services/UserTimeline/UserTimeline.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.OpenApi.Models;
+using UserTimeline.Application.Contracts.Persistence;
+using UserTimeline.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+
+builder.Services.AddSwaggerGen(options => {
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "User timeline API", Version = "v1",
+        Description = "Receives new Kweets by users through a message queue and updates the timeline in the background"
+        });
+    }
+);
+
+
+// TODO: Consider moving to smaller register function like in the Kweet API.
+builder.Services.AddScoped<IUserTimelineRepository, UserTimelineRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
